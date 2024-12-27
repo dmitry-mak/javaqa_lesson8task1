85b7470bf8912730558d6a133e511026bfab2314
dmitry-mak <akkaru@gmail.com>
2024-12-27 14:36:29 +0200

    Jacoco plugin set up; pom.xml additional configuration; 100% tests coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -3,7 +3,6 @@ package ru.netology.statistic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
@@ -13,6 +12,19 @@ class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void findMaxFirstNegative() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-1, -2, 0, 2};
+        long expected = 2;
 
         long actual = service.findMax(incomesInBillions);
 
